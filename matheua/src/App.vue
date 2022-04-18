421502bd500705b3890b8f4d79ed59ec88e4633d
Victor Luiz da Silva <99228756+VictorLDS@users.noreply.github.com>
2022-04-18 09:29:10 -0300

    Add files via upload

---
+++ matheua/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import BlocoCabecalho from "./components/BlocoCabecalho.vue";
+import BlocoPrincipal from "./components/BlocoPrincipal.vue";
+import BlocoFooter from "./components/BlocoFooter.vue";
+export default {
+  components: { MenuLateral, BlocoCabecalho, BlocoPrincipal, BlocoFooter },
+};
+</script>
+
+<template>
+  <MenuLateral />
+  <BlocoCabecalho />
+  <BlocoPrincipal />
+  <BlocoFooter />
+</template>
+
+<style></style>
